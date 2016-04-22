b3a3708109253472eaf8338c70028cf6e373c195
bensoer <ben@soernet.ca>
2016-04-22 15:24:45 -0700

    added stubs for udp sockets

--- lib/udp/UDPSocket.cpp
+++ lib/udp/UDPSocket.cpp
@@ -5,3 +5,30 @@
 #include "UDPSocket.h"
 
 using namespace terrisock;
+
+UDPSocket::UDPSocket() {
+
+
+}
+
+int UDPSocket::getSocket() {
+
+}
+
+void UDPSocket::setSocket(int socket) {
+
+}
+
+void UDPSocket::close() {
+
+}
+
+void UDPSocket::shutdown(int how) {
+
+}
+
+void UDPSocket::shutdown() {
+
+
+
+}
--- lib/udp/UDPSocket.h
+++ lib/udp/UDPSocket.h
@@ -5,9 +5,20 @@
 #ifndef TERRISOCK_UDPSOCKET_H
 #define TERRISOCK_UDPSOCKET_H
 
-namespace terrisock {
-    class UDPSocket {
+#include <Socket.h>
 
+namespace terrisock {
+    class UDPSocket: public Socket {
+
+    public:
+        UDPSocket();
+
+        int getSocket();
+        void setSocket(int socket);
+
+        virtual void shutdown();
+        virtual void shutdown(int how);
+        virtual void close();
     };
 }
 
