b72e045c05e82d6623e5d69161fa5380db2eec84
bensoer <ben@soernet.ca>
2016-04-28 12:25:08 -0700

    added a license

--- tests/simulations/udp_client.cpp
+++ tests/simulations/udp_client.cpp
@@ -13,25 +13,6 @@ using namespace terrisock;
 
 int main(){
 
-    /*
-    TCPSocket * socket = new TCPSocket();
-
-    SocketAddress * address = new SocketAddress("localhost", 8001);
-
-    socket->connect(address);
-
-    cout << "Sending Greeting" << endl;
-    long bytesSent = socket->send("{He}}llo Wor\\\\ld!}");
-
-    cout << "Bytes sent: " << to_string(bytesSent) << endl;
-
-    socket->shutdown();
-    socket->close();
-
-    delete(address);
-    delete(socket);
-
-    */
 
     cout << "Setting Up UDP Socket" << endl;
     UDPSocket * socket = new UDPSocket();
