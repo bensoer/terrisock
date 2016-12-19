170b710414dfe106b079cfa04ea9e0cb2f9d31e5
bensoer <ben@soernet.ca>
2016-12-19 10:52:00 -0800

    started on tcp filling in

--- lib/raw/headers/TCPHeader.cpp
+++ lib/raw/headers/TCPHeader.cpp
@@ -2,4 +2,15 @@
 // Created by bensoer on 27/10/16.
 //
 
+#include <cstddef>
+#include <cstring>
 #include "TCPHeader.h"
+
+using namespace terrisock;
+
+TCPHeader::TCPHeader() {
+    size_t size = sizeof(tcphdr);
+    this->payload = new char[size];
+    memset(this->payload,0,size);
+    this->tcp = (tcphdr *)this->payload;
+}
--- lib/raw/headers/TCPHeader.h
+++ lib/raw/headers/TCPHeader.h
@@ -6,9 +6,35 @@
 #define TERRISOCK_TCPHEADER_H
 
 
-class TCPHeader {
+#include "Payload.h"
+#include <netinet/tcp.h>
 
-};
+namespace terrisock {
+
+    class TCPHeader: public Payload {
+
+    private:
+        char * payload = nullptr;
+        struct tcphdr * tcp = nullptr;
+    public:
+
+        TCPHeader();
+
+        void setSourcePort(short sourcePort);
+        void setDestinationPort(short destinationPort);
+
+        void setSequenceNumber(int sequenceNumber);
+        void setAcknowledgementNumber(int acknowledgementNumber);
+
+        void setWindowSize(short windowSize);
+        void setChecksum(short checksum);
+
+        void setUrgentPointer(short urgentPointer);
+
+
+    };
+}
+
 
 
 #endif //TERRISOCK_TCPHEADER_H
